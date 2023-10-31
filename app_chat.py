e525f53212ca6bc554854e638d7d97453c5d099d
Inteligência Mil Grau <48864772+inteligenciamilgrau@users.noreply.github.com>
2023-10-31 15:33:47 -0300

    Versão Básica Inicial do Assistente Web
    Primeira Versão Web do Assistente Mil Grau

---
+++ app_chat.py
@@ -0,0 +1,160 @@
+import time
+from flask import Flask, render_template, request, jsonify
+import openai
+import pyttsx3
+import threading
+import json
+import os
+
+app = Flask(__name__)
+
+# Check if the config.json file exists
+if os.path.exists('config.json'):
+    # Read the JSON data from the existing file
+    with open('config.json', 'r') as file:
+        json_data = json.load(file)
+else:
+    # If the file doesn't exist, create it with specific content
+    initial_data = [
+        {
+            "model": "gpt-3.5-turbo",
+            "api_key": "sua-api-key-openai",
+            "assistente_falante": False
+        }
+    ]
+
+    with open('config.json', 'w') as file:
+        json.dump(initial_data, file, indent=4)
+
+    # Set the initial data to the json_data variable
+    json_data = initial_data
+
+# Access the values and store them in separate variables
+model = json_data[0]["model"]
+api_key = json_data[0]["api_key"]
+falar_texto = json_data[0]["assistente_falante"]
+
+# Now you have two variables, model and api_key, containing the values from the JSON
+print("Model:", model)
+print("API Key:", api_key)
+print("Falar Texto:", falar_texto)
+
+openai.api_key = api_key
+
+if api_key == "sua-api-key-openai":
+    print("Coloque a sua chave no arquivo config.json")
+
+
+
+image_file = "01_chatbot_feliz.png"
+
+def thread_falar(resposta_t, voz):
+    velocidade = 180
+    engine = pyttsx3.init()
+    engine.setProperty('rate', velocidade)
+    voices = engine.getProperty('voices')
+    for indice, vozes in enumerate(voices):  # listar vozes
+        print(indice, vozes.name)
+        pass
+    engine.setProperty('voice', voices[voz].id)
+
+    start = time.time()
+    engine.say(resposta_t)
+    print("Falando")
+    engine.runAndWait()
+    end = time.time()
+    print("Duracao", end - start)
+
+def generate_answer(messages):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=1.0,
+        )
+        return response
+    except Exception as e:
+        print("Erro", e)
+        return e
+
+@app.route('/')
+def assistente_mil_grau():
+    return render_template('index_chat.html')
+
+
+@app.route('/assistente.html')
+def assistente():
+    return render_template('assistente.html', falar_texto=falar_texto)
+
+
+@app.route('/avatar.html')
+def avatar():
+    return render_template('avatar.html', image_file=image_file)
+
+
+@app.route('/update_image', methods=['GET', 'POST'])
+def update_image():
+    # Update the image file variable based on some condition
+    # In this example, we toggle between two images
+    global image_file
+    image_file = "02_chatbot_falando.png" if image_file == "01_chatbot_feliz.png" else "01_chatbot_feliz.png"
+
+    # Return the new image file path
+    return jsonify(image_file)
+
+@app.route('/actual_image_file', methods=['GET', 'POST'])
+def actual_image_file():
+    # Return the new image file path
+    return jsonify({"img": image_file})
+
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    global image_file
+
+    data = request.get_json()
+
+    if falar_texto:
+        pergunta = data["userText"][-1]["content"]
+        pergunta_thread = threading.Thread(target=thread_falar, args=(pergunta, 7))
+        pergunta_thread.start()
+    response = generate_answer(data["userText"])
+
+    if falar_texto:
+        pergunta_thread.join()
+    try:
+        resposta = response.choices[0].message.content
+    except:
+        resposta = """
+        Problemas Técnicos! Tente de novo ou faça uma gambiarra boa! Não esqueça de colocar sua Chave da OpenAI no arquivo Config!
+        """
+
+    if falar_texto:
+        falar_thread = threading.Thread(target=thread_falar, args=(resposta, 1))
+        falar_thread.start()
+
+        while falar_thread.is_alive():
+            update_image()
+            time.sleep(0.18)
+            print("falando")
+        image_file = "02_chatbot_falando.png"
+        update_image()
+
+
+    return resposta
+
+@app.route('/falar', methods=['GET'])
+def falar():
+    global falar_texto
+    falar_texto = bool(request.args.get('falar'))
+
+    json_data[0]["assistente_falante"] = falar_texto
+
+    # Save the updated JSON data back to the file
+    with open("config.json", "w") as file:
+        json.dump(json_data, file, indent=4)
+
+    return {"ok":"Ok"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,192 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const deleteButton = document.querySelector("#delete-btn");
+
+let userText = null;
+let userHistory = [];
+
+const defaultText = () => {
+
+  chatContainer.innerHTML = `<div class="default-text">
+  <p>Assistente Mil Grau</p>
+</div>`;
+
+  userHistory = [{ role: "system", content: "Seu nome é Assistente Mil Grau"}];
+
+  chatInput.disabled = false;
+
+};
+
+const createChatElement = (content, className) => {
+  // Create new div and apply chat, specified class and set html content of div
+  const chatDiv = document.createElement("div");
+  chatDiv.classList.add("chat", className);
+  chatDiv.innerHTML = content;
+  return chatDiv; // Return the created chat div
+};
+
+const getChatResponse = async (incomingChatDiv) => {
+  const pElement = document.createElement("p");
+
+  userHistory.push({role: "user", content: userText});
+
+  try {
+    response = ""
+    await ($.ajax({
+        type: 'POST',
+        url: '/enviar',
+        contentType: 'application/json',
+        data: JSON.stringify({ userText: userHistory }),
+        success: function(response_gpt) {
+            response = response_gpt
+        }
+    }));
+
+    pElement.textContent = response;
+    userHistory.push({role: "assistant", content: response});
+
+  } catch (error) {
+    // Add error class to the paragraph element and set error text
+    pElement.classList.add("error");
+    pElement.textContent =
+      "Deu ruim no assistente! Tenta de novo!";
+  }
+
+  // Remove the typing animation, append the paragraph element and save the chats to local storage
+  incomingChatDiv.querySelector(".typing-animation").remove();
+  incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+
+  if (document.querySelectorAll(".chat-container .outgoing").length >= 1000) {
+    chatInput.disabled = true;
+
+    let html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="static/img/chatbot.png" alt="chatbot-img">
+                        <p class='error'>Chegou ao limite de texto (<a href='#' onclick=\"document.querySelector('#delete-btn').click()\">Limpar</a>)</p>
+                    </div>
+                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
+                </div>`;
+  let incomingChatDiv = createChatElement(html, "incoming");
+  chatContainer.appendChild(incomingChatDiv);
+
+  }
+
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+};
+
+const copyResponse = (copyBtn) => {
+  // Copy the text content of the response to the clipboard
+  const reponseTextElement = copyBtn.parentElement.querySelector("p");
+  navigator.clipboard.writeText(reponseTextElement.textContent);
+  copyBtn.textContent = "done";
+  setTimeout(() => (copyBtn.textContent = "content_copy"), 1000);
+};
+
+const showTypingAnimation = () => {
+  // Display the typing animation and call the getChatResponse function
+  const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="static/img/chatbot.png" alt="chatbot-img">
+                        <div class="typing-animation">
+                            <div class="typing-dot" style="--delay: 0.2s"></div>
+                            <div class="typing-dot" style="--delay: 0.3s"></div>
+                            <div class="typing-dot" style="--delay: 0.4s"></div>
+                        </div>
+                    </div>
+                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
+                </div>`;
+  // Create an incoming chat div with typing animation and append it to chat container
+  const incomingChatDiv = createChatElement(html, "incoming");
+  chatContainer.appendChild(incomingChatDiv);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  getChatResponse(incomingChatDiv);
+};
+
+const handleOutgoingChat = () => {
+  userText = chatInput.value.trim(); // Get chatInput value and remove extra spaces
+  if (!userText) return; // If chatInput is empty return from here
+
+  // Clear the input field and reset its height
+  chatInput.value = "";
+
+  const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="static/img/user.png" alt="user-img">
+                        <p>${userText}</p>
+                    </div>
+                </div>`;
+
+  // Create an outgoing chat div with user's message and append it to chat container
+  const outgoingChatDiv = createChatElement(html, "outgoing");
+  chatContainer.querySelector(".default-text")?.remove();
+  chatContainer.appendChild(outgoingChatDiv);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  setTimeout(showTypingAnimation, 500);
+};
+
+deleteButton.addEventListener("click", () => {
+  // Remove the chats from local storage and call loadDataFromLocalstorage function
+  if (confirm("Apagar tudo?")) {
+    defaultText();
+  }
+});
+
+const initialInputHeight = chatInput.scrollHeight;
+
+chatInput.addEventListener("input", () => {
+  // Adjust the height of the input field dynamically based on its content
+  chatInput.style.height = `${initialInputHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && !e.shiftKey) {
+    e.preventDefault();
+    handleOutgoingChat();
+  }
+});
+
+defaultText();
+sendButton.addEventListener("click", handleOutgoingChat);
+
+// Obtenha a referência à checkbox usando o id
+var minhaCheckbox = document.getElementById("minhaCheckbox");
+
+/* Verifique se a checkbox está marcada
+if (minhaCheckbox.checked) {
+    console.log("A checkbox está marcada.");
+} else {
+    console.log("A checkbox não está marcada.");
+}
+*/
+
+// Adicione um ouvinte de evento para detectar alterações na checkbox
+minhaCheckbox.addEventListener("change", function() {
+    falar = ""
+    if (minhaCheckbox.checked) {
+        //console.log("A checkbox foi marcada.");
+        falar = "true"
+    } else {
+        //console.log("A checkbox foi desmarcada.");
+    }
+
+    fetch('/falar?falar=' + falar, {
+      method: 'GET', // This is the default, so you can omit it.
+      headers: {
+        'Content-Type': 'application/json', // Set the content type if needed.
+        // You can add other headers here as needed.
+      },
+      // You can add additional options like credentials, mode, etc.
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Parse the response as JSON
+    })
+    .catch(error => {
+      // Handle errors here
+      console.error('Fetch error:', error);
+    });
+});
